81d0d1f7a7ceb64b62a315947df09a3b00efd343
Tyler Walters <git@tylerwalts.com>
2021-02-21 12:47:45 -0800

    Added S3 bucket lifestyle for raw emails.

--- lib/ses-stack.ts
+++ lib/ses-stack.ts
@@ -13,7 +13,22 @@ export class SesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, `${config.project}SESBucket`);
+    const bucket = new s3.Bucket(this, `${config.project}SESBucket`, {
+        lifecycleRules: [
+          {
+            transitions: [
+                {
+                    storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+                    transitionAfter: cdk.Duration.days(30),
+                },
+                {
+                    storageClass: s3.StorageClass.GLACIER,
+                    transitionAfter: cdk.Duration.days(90),
+                },
+            ],
+          },
+        ],
+    });
 
     const forwarderLambda = new lambda.Function(this, `${config.project}SESForwarder`, {
       runtime: lambda.Runtime.NODEJS_12_X,
